9def2e91d07cf97615f185bd851c1b66c05c3708
favbri1 <fabricioalvarezl0403@gmail.com>
2025-06-07 17:46:14 -0400

    creando la app libros favbri1

--- intercambio/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class IntercambioConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'intercambio'
--- intercambio_libros/intercambio_libros/wsgi.py
+++ intercambio_libros/intercambio_libros/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'intercambio_libros.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "intercambio_libros.settings")
 
 application = get_wsgi_application()
