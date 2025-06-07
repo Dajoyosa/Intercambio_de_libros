4e2e6a6d693effc2eddb9fb9a649773a62fd099b
favbri1 <fabricioalvarezl0403@gmail.com>
2025-06-07 18:15:58 -0400

    añadiendo las tablas para libros en models.py favbri1y

--- intercambio_libros/libros/models.py
+++ intercambio_libros/libros/models.py
@@ -1,3 +1,32 @@
+from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
 
-# Create your models here.
+class Usuario(AbstractUser):
+    direccion = models.CharField(max_length=255)
+    telefono = models.CharField(max_length=15)
+    groups = models.ManyToManyField(Group, related_name="usuario_groups")
+    user_permissions = models.ManyToManyField(Permission, related_name="usuario_permissions")
+
+class Libro(models.Model):
+    titulo = models.CharField(max_length=255)
+    autor = models.CharField(max_length=255)
+    genero = models.CharField(max_length=100)
+    estado = models.CharField(max_length=50)  # "Nuevo", "Usado - Buen estado", etc.
+    propietario = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name="libros")
+    def __str__(self):
+        return {self.titulo}
+
+class Intercambio(models.Model):
+    solicitante = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name="solicitudes")
+    propietario = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name="ofertas")
+    libro_solicitado = models.ForeignKey(Libro, on_delete=models.CASCADE, related_name="intercambios")
+    estado = models.CharField(max_length=20, choices=[("Pendiente", "Pendiente"), ("Aceptado", "Aceptado"), ("Rechazado", "Rechazado")])
+
+class Historial(models.Model):
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    libro = models.ForeignKey(Libro, on_delete=models.CASCADE)
+    fecha_intercambio = models.DateTimeField(auto_now_add=True)
+    comentario = models.TextField(null=True, blank=True)
+    def __str__(self):
+        return {self.usuario.username}
+
