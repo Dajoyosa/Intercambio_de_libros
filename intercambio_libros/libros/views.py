a7b20012fc90fa85dd7c2992d6206e7a7577f3b7
Bar <barbabricq@gmail.com>
2025-06-08 19:51:19 -0400

    Agregue urls, hice el login el logout y el form de registro

--- intercambio_libros/libros/views.py
+++ intercambio_libros/libros/views.py
@@ -1,4 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .forms import RegistroUsuarioForm
 
 # Create your views here.
+def registro_view(request):
+    if request.method == 'POST':
+        form = RegistroUsuarioForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('inicio')  # o la ruta que tengas definida
+    else:
+        form = RegistroUsuarioForm()
+    return render(request, 'registro.html', {'form': form})
+
+
 
