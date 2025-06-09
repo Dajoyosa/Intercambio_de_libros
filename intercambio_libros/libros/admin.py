e4abad7c435e2bbfee7c2bec8b5aa1ecb8d64109
Bar <barbabricq@gmail.com>
2025-06-09 17:24:54 -0400

    Extendiendo el modelo usuario, configuracion general del proy

--- intercambio_libros/libros/admin.py
+++ intercambio_libros/libros/admin.py
@@ -1,12 +1,7 @@
 from django.contrib import admin
-from .models import Usuario
-
-# Register your models here.
-
-admin.site.register(Usuario)
-
 from .models import Usuario, Libro, Intercambio, Historial
 # Register your models here.
+
 admin.site.register(Usuario)
 admin.site.register(Libro)
 admin.site.register(Intercambio)
