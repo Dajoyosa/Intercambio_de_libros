43e73d4363d2d888884b72426cad59b564594728
Alejandro Sanchez <alexsanchez3480@gmail.com>
2025-06-09 00:39:54 -0400

    agrego las importaciones a el urls.py de la app

--- intercambio_libros/libros/urls.py
+++ intercambio_libros/libros/urls.py
@@ -1,15 +1,6 @@
-from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
-from Libro import views
-
+from django.urls import path
+from . import views
 
 urlpatterns = [
-        path('admin/', admin.site.urls),
-    path('', include('django.contrib.auth.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('', views.inicio, name='inicio'),
-    path('registro/', views.registro, name='registro'),
-    path('libros/', include('Libro.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+]
