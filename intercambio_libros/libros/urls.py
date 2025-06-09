cc3ebc708d8dcce75b1c88dec73ecf7af459268c
Alejandro Sanchez <alexsanchez3480@gmail.com>
2025-06-09 00:31:55 -0400

    prueba

--- intercambio_libros/libros/urls.py
+++ intercambio_libros/libros/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path
-from django.contrib.auth import views as auth_views
-from . import views
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from Libro import views
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
